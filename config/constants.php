b3be15a2435d17be9d0646ca3d737c1b5b76f418
ironman <stark@ironmans-MacBook-Air.local>
2023-07-06 19:00:18 +0530

    saved

--- config/constants.php
+++ config/constants.php
@@ -4,7 +4,7 @@
 
 
     // //Create Constants to Store Non Repeating Values
-    // define('SITEURL', 'http://localhost/food-order/');
+    define('SITEURL', 'https://localhost/develop/');
     define('LOCALHOST', 'localhost');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
